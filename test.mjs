00f993d59b8e91f7cd532c4e430f06dcf8cae9d1
jsoncode <jsoncode@example.com>
2022-01-11 14:33:07 +0800

    添加山西省

--- test.mjs
+++ test.mjs
@@ -36,7 +36,7 @@ list.push(getLastInfo({
     sort: 3,
     url: 'http://wsjk.tj.gov.cn/XWZX6600/newyqfkdt/',
 
-    listReg: /<a href='([^']+)'[^<>]*>\s*([^<>]*\d+年\d+月\d+日[^<>]*?)[\s\t\r]*<\/a>/i,
+    listReg: /<a href=['"]([^'"]+)['"][^<>]*>\s*(\d+年\d+月\d+日[^<>]*?)[\s\t\r]*<\/a>/i,
     contentReg: /<div class="view TRS_UEDITOR trs_paper_default trs_web">([\s\S]+?)<\/div>/i,
     timeReg: /<div class="page_time">发布日期：([\s\S]+?)<\/div>/
 }))
@@ -48,6 +48,15 @@ list.push(getLastInfo({
     listReg: /<ul class="list-group listmain">\s*<li class="list-group-item">\s*<a href="([^"]+)" target="_blank">\s*([^<>]+)\s*<\/a>/i,
     contentReg: /<div id="artibody" style=" margin:0 30px; padding:10px;line-height:200%">([\s\S]+?)<\/div>/i,
     timeReg: /\s*时间：([^\s<>：]+)\s*<span id="divResulta">/
+}))
+// 陕西省卫健委
+list.push(getLastInfo({
+    name: '陕西省卫健委 疫情通报',
+    sort: 4,
+    url: 'http://sxwjw.shaanxi.gov.cn/sy/wjyw/',
+    listReg: /<ul class="cm-news-list gl-news-list">\s*<li class="clearfix">\s*<a href="([^"]+)" target="_blank" title="([^"]*?确诊病例[^"]*)"/i,
+    contentReg: /<div class="view TRS_UEDITOR trs_paper_default trs_word">([\s\S]+?)<\/div>/i,
+    timeReg: /<span class="cm-con">\s*([^<>]+)\s*<\/span>/
 }))
 
 await buildFile(list, './public/data.json')
